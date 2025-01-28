5a50ccd899666cb24d77623020ad51b95f4f42a9
juanpink <jmiopin6@gmail.com>
2025-01-28 09:52:24 +0100

    genero lista para pruebas

--- src/app/pages/buscar-tareas/buscar-tareas.page.ts
+++ src/app/pages/buscar-tareas/buscar-tareas.page.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonAvatar, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonAvatar, IonButtons, IonContent, IonHeader, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ITarea } from 'src/app/interfaces/itarea';
+import { TareasService } from 'src/app/services/tareas.service';
 
 @Component({
   selector: 'app-buscar-tareas',
   templateUrl: './buscar-tareas.page.html',
   styleUrls: ['./buscar-tareas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonList, IonItemSliding, IonIcon, IonItemOption, IonItemOptions, IonLabel, IonAvatar, IonItem]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonList, IonItemSliding, IonItemOption, IonItemOptions, IonLabel, IonAvatar, IonItem]
 })
 export class BuscarTareasPage implements OnInit {
-
+  gestorTareas = inject(TareasService);
+  private tareasFiltradas : Array<ITarea> = this.gestorTareas.getTareas();
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  verEstado(estado : string) {
+    console.log("VER ESTADO --> ", estado);
+    console.log("Tareas --> ", this.tareasFiltradas)
   }
 
 }
--- src/app/services/tareas.service.ts
+++ src/app/services/tareas.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ITarea } from '../interfaces/itarea';
+import { Criticidad, ITarea } from '../interfaces/itarea';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareasService {
-  private tareas : Array<ITarea> = [];
+  //private tareas : Array<ITarea> = [];
+  private tareas : Array<ITarea> = [
+    {"nombre": "Juanma", "descripcion": "comprar pan", "fechaLimite": "2025-01-28", "criticidad": Criticidad.ALTA, "estado": "Realizada"},
+    {"nombre": "Lucia", "descripcion": "recoger coche", "fechaLimite": "2025-01-31", "criticidad": Criticidad.MEDIA, "estado": "Pendiente"},
+    {"nombre": "Carlos", "descripcion": "ir al médico", "fechaLimite": "2025-02-27", "criticidad": Criticidad.BAJA, "estado": "Pendiente"}
+  ];
 
   constructor() { }
 
